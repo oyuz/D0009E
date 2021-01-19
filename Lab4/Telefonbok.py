016375f02292e137797a1fd5406e3dcf8abc1f1b
smerdyakov <73742467+smerdyakov@users.noreply.github.com>
2021-01-19 12:14:36 +0100

    Lab 4

--- Lab4/Telefonbok.py
+++ Lab4/Telefonbok.py
@@ -1,3 +1,18 @@
+class OccupiedException(Exception):
+    def __init__(self, message):
+        print(message, "already exists")
+        
+
+class NotFoundException(Exception):
+    def __init__(self, message):
+        print(message, "not found")
+        
+
+class AliasException(Exception):
+    def __init__(self):
+        print("name not found or duplicate name")
+
+
 class Number:
     def __init__(self, number):
         self.number = number
@@ -7,65 +22,42 @@ class Number:
 
     def setNumber(self, number):
         self.number = number
-    
+        
 
 class Telefonbok:
     def __init__(self):
         self.dic = {}
-        self.cmd = {
-            "add" : self.add,
-            "lookup" : self.lookup,
-            "alias" : self.alias,
-            "change" : self.change,
-            "save" : self.save,
-            "load" : self.load,
-            "quit" : self.quit
-            }
-        self.prompt()
-        
 
-    def prompt(self):
-        while True:
-            inp = input(("telebok> ")).split()
-            try:
-                self.cmd[inp[0]](*inp[1:])
-            except TypeError:
-                print("Invalid command")
-            except SystemExit:
-                return
-            
     def add(self, name, number):
         if name in self.dic:
-            print(name, "already exists")
-            return
-        if self.number_available(number):
-            self.dic[name] = Number(number)
-    
+            raise OccupiedException(name)
+        if not self.number_available(number):
+            raise OccupiedException(number)
+        self.dic[name] = Number(number)
+              
+    def number_available(self, number):
+        for num in self.dic.values():
+            if num.getNumber() == number:
+                return False
+        return True
+
     def lookup(self, name):
         try:
             print(self.dic[name].getNumber())
         except KeyError:
-            print(name, "not found")
+            raise NotFoundException(name)
 
     def alias(self, name, alias):
         if alias in self.dic or name not in self.dic:
-            print("name not found or duplicate name")
-        else:
-            self.dic[alias] = self.dic[name]
+            raise AliasException
+        self.dic[alias] = self.dic[name]
 
     def change(self, name, number):
         if name not in self.dic:
-            print(name, "not found")
-            return
-        if self.number_available(number):
-            self.dic[name].setNumber(number)
-
-    def number_available(self, number):
-        for num in self.dic.values():
-            if num.getNumber() == number:
-                print(number, "already exists")
-                return False
-        return True
+            raise NotFoundException(name)
+        if not self.number_available(number):
+            raise OccupiedException(number)
+        self.dic[name].setNumber(number)
 
     def save(self, file):
         f = open(file, 'w')
@@ -83,5 +75,44 @@ class Telefonbok:
 
     def quit(self):
         raise SystemExit
+            
 
-t = Telefonbok()
+class TelefonbokUI:
+    def __init__(self):
+        self.book = Telefonbok()
+        self.cmd = {
+            "add" : self.book.add,
+            "lookup" : self.book.lookup,
+            "alias" : self.book.alias,
+            "change" : self.book.change,
+            "save" : self.book.save,
+            "load" : self.book.load,
+            "quit" : self.book.quit
+            }
+
+    def run(self):
+        while True:
+            inp = input(("telebok> ")).split()
+            try:
+                self.cmd[inp[0]](*inp[1:])
+            except TypeError:
+                print("Invalid command")
+            except IndexError:
+                print("Invalid command")
+            except KeyError:
+                print("Invalid command")
+            except OccupiedException:
+                pass
+            except NotFoundException:
+                pass
+            except AliasException:
+                pass
+            except SystemExit:
+                return
+
+def main():
+    tui = TelefonbokUI()
+    tui.run()
+
+if __name__ == "__main__":
+    main()
