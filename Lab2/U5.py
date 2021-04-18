5e6e31d9aa5c3cca4c9b35469ff3afffa133d585
smerdyakov <73742467+smerdyakov@users.noreply.github.com>
2021-04-18 17:26:02 +0200

    haskell

--- Lab2/U5.py
+++ Lab2/U5.py
@@ -2,14 +2,7 @@ def derivative(f, x, h):
     return (f(x+h)-f(x-h))/(2*h)
 
 def solve(f, x0, h):
-  while True:
     x=x0-f(x0)/derivative(f,x0,h)
     if abs(x-x0)<h:
       return x
-    x0=x
-
-def f1(x):
-    return x**2-1
-
-def f2(x):
-    return 2**x-1 
+    return solve(f, x, h)
